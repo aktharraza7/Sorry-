6c8018f882c77f0446d8dc883652b19044a24902
aktharraza7 <looserxar7@gmail.com>
2026-07-05 21:03:06 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'apology-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/script.js'];
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
